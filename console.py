5c81ec204e28bc2c8db9e13e78532cc4756cf810
AteCastillo <atecast@gmail.com>
2021-02-17 16:30:09 -0300

    fix console

--- console.py
+++ console.py
@@ -25,9 +25,11 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, line):
         """Quit command to exit the program"""
+        print("")
         return True
 
     def emptyline(self):
+        """Do nothing with an empty line"""
         pass
 
     def do_create(self, line):
@@ -45,22 +47,19 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, line):
         """Prints the string representation of an instance
         based on the class name and id"""
-        if not line:
+        objs = storage.all()
+        if not line or len(line) == 0:
             print('** class name missing **')
         else:
             arg = line.split()
             if not arg[0] in HBNBCommand.class_list:
-                print('** class doesn\'t exist **')
+                print("** class doesn't exist **")
+            elif len(arg) == 1:
+                print('** instance id missing **')
+            elif (arg[0] + "." + arg[1]) not in objs:
+                print('** no instance found **')
             else:
-                if len(arg) > 1:
-                    class_key = ''
-                    class_key = arg[0] + '.' + arg[1]
-                    if class_key in storage.all():
-                        print(storage.all()[class_key])
-                    else:
-                        print('** no instance found **')
-                else:
-                    print('** instance id missing **')
+                    print(objs[arg[0] + "." + arg[1]])
 
     def do_all(self, line):
         '''
@@ -77,25 +76,26 @@ class HBNBCommand(cmd.Cmd):
         elif arg[0] in HBNBCommand.class_list:
             for class_key in objs:
                 if arg[0] in class_key:
-                    new_list.append(str(objs[class_key]).__str__())
+                    new_list.append(objs[class_key].__str__())
             print(new_list)
         else:
-            print('** class doesn\'t exist **')
+            print("** class doesn't exist **")
 
     def do_destroy(self, line):
         """Deletes an instance
         based on the class name and id"""
         objs = storage.all()
-        if line != "":
+        if line != "" or len(line) != 0:
             args = line.split()
             if args[0] not in HBNBCommand.class_list:
                 print("** class doesn't exist **")
-            if len(args) == 1:
+            elif len(args) == 1:
                 print("** instance id missing **")
-            if len(args) == 2:
+            elif len(args) == 2:
                 key = args[0] + "." + args[1]
                 if key in objs:
-                    objs.pop(key)
+                    del objs[key]
+                    storage.save()
                 else:
                     print("** no instance found **")
         else:
@@ -105,32 +105,33 @@ class HBNBCommand(cmd.Cmd):
         """updates an instance based on the class name and id"""
         objs = storage.all()
         scape = ["\"", "\'"]
-        if line != "":
+        if line != "" or len(line) != 0:
             args = line.split()
             if args[0] not in HBNBCommand.class_list:
                 print("** class doesn't exist **")
             if len(args) == 1:
                 print("** instance id missing **")
+                return False
             if len(args) == 2:
                 print("** attribute name missing **")
             if len(args) == 3:
                 print("** value missing **")
+                return False
             if len(args) >= 4:
                 key = args[0] + "." + args[1]
                 if key in objs:
                     if args[2] not in HBNBCommand.attr_list:
-                        if args[3].startswith(("\"", "'")) and args[3].endswith(("\"", "'")):
+                        if args[3][0] in scape and args[3][-1] in scape:
                             setattr(objs[key], args[2], str(args[3][1:-1]))
                         else:
                             setattr(objs[key], args[2], str(args[3]))
                         storage.save()
-                    else:
-                        print("No se puede editar")
                 else:
                     print("** no instance found **")
+                    return False
         else:
             print("** class name missing **")
-    
+
     def count(self, line):
         """Return the amount of instances of the class"""
         count = 0
@@ -141,6 +142,24 @@ class HBNBCommand(cmd.Cmd):
                     count += 1
             print(count)
 
+    def default(self, line):
+        """In case to not found the command this func is executed"""
+        functions = {"all()": HBNBCommand.do_all, "count()": HBNBCommand.count}
+        try:
+            args = line.split(".")
+            func = ""
+            func_id = ""
+            for index, letter in enumerate(args[1]):
+                if letter == "(":
+                    func = args[1][0:index] + "()"
+                    func_id = args[1][index + 2: -2]
+                    break
+            if func in functions:
+                functions[func](self, args[0])
+            else:
+                print("*** Unknown syntax: {}".format(line))
+        except:
+            print("*** Unknown syntax: {}".format(line))
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
